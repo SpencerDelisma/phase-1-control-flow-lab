15f9ab0b20f00c9ad98d843a4b19d9ed0179a290
Delisma <delispence7@gmail.com>
2024-02-02 05:53:48 -0500

    added

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//function scuberGreetingForFeet(custerRide){
+  //let result
+  function scuberGreetingForFeet(customerRide){
+    if (customerRide <= 400) {
+      return "This one is on me!";
+    } else if (customerRide > 400 && customerRide <= 2000) {
+      return "That will be twenty bucks.";
+    } else if (customerRide > 2000 && customerRide <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else if (customerRide > 2500) {
+      return "No can do.";
+    }
+  }
+
+  
+    
+  
+
+
+function ternaryCheckCity(City){
+   
+  if(City == "NYC"){
+    return "Ok, sounds good."
+}else{
+  return "No go."
+}
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ if(tip == "generous"){
+   return "Thank you so much."
+ }else if(tip === "not as generous"){
+    return "Thank you."
+ }else{
+   return "Bye."
+ }
+  
+   
+ 
 }
